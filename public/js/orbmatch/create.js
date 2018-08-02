979165fa37543625d0e15f19bef235fb11d4d9eb
dustinpfister <dustin.pfister@gmail.com>
2018-08-02 10:53:27 -0400

    pouch

--- public/js/orbmatch/create.js
+++ public/js/orbmatch/create.js
@@ -2,29 +2,41 @@ game.state.add('create', {
 
     create: function () {
 
-       // using fly graphics
-        var fly = new FlyGFX.GFX({
+        // using fly graphics
+        var flyGrid = new FlyGFX.GFX({
 
                 game: this.game,
-                width: 4,
-                pxSize: 8,
-                palette: [0x000000, 0xffffff],
-                layers: [[1, 0, 0, 0,
-                          0, 1, 0, 0,
-                          0, 0, 1, 0,
-                          0, 0, 0, 1]]
+                width: 32 * 4,
+                pxSize: 1,
+                palette: [0x000000, 0x4a4a4a, 0x8a8a8a],
+                layers: [function (x, y, i) {
+
+                        if (x % 32 === 0 || y % 32 === 0) {
+
+                            return 2;
+
+                        }
+
+                        return 1;
+
+                    }
+                ]
 
             });
 
         // making sprite sheet with fly graphics
-        var bxSheet = fly.generateSheet({
-                key: 'bx'
+        var bxSheet = flyGrid.generateSheet({
+                key: 'sheet_pouch'
             });
 
         // adding sprite with sheet
-        var sprite = game.add.sprite(10, 10, 'bx', 0);
+        var pouch = game.add.sprite(0, 0, 'sheet_pouch', 0);
+
+        console.log(game.data);
+		
+		
+		
 
     }
 
 });
-

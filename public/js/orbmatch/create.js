2707f41889ca8adfe7c2d0232e7aa445923565d3
dustinpfister <dustin.pfister@gmail.com>
2018-08-02 11:21:57 -0400

    craft sprite

--- public/js/orbmatch/create.js
+++ public/js/orbmatch/create.js
@@ -1,5 +1,17 @@
 (function () {
 
+    // create main game data object if it is not there
+    game.data = game.data || {};
+
+    // data object for create state
+    game.data.create = {
+
+        pouch: null, // what will be a ref to the pouch sprite
+        craft: null, // what will be a ref to the craft sprite
+
+    };
+
+    // make the Pouch that will hold current ORBS
     var mkPouch = function () {
 
         // using fly graphics
@@ -23,10 +35,11 @@
         });
 
         // sprite for pouch
-        var pouch = game.add.sprite(50, 50, 'sheet_pouch', 0);
-        pouch.inputEnabled = true;
+        var pouch = game.data.create.pouch = game.add.sprite(32, 64, 'sheet_pouch', 0);
+        pouch.name = 'pouch';
 
         // pouch events
+        pouch.inputEnabled = true;
         pouch.events.onInputDown.add(function (pouch, pt) {
 
             // x, and y relative to pouch
@@ -34,6 +47,49 @@
             y = pt.y - pouch.y;
 
             console.log(x, y);
+			console.log(pouch.name);
+
+        });
+
+    };
+
+    // make the craft sprite that will be used to create sprites
+    var mkCraft = function () {
+
+        // using fly graphics
+        var flyCraft = new FlyGFX.GFX({
+                game: this.game,
+                width: 32 * 3,
+                pxSize: 1,
+                palette: [0x000000, 0x4a4a4a, 0x8a8a8a],
+                layers: [function (x, y, i) {
+                        if (x % 32 === 0 || y % 32 === 0) {
+                            return 2;
+                        }
+                        return 1;
+                    }
+                ]
+            });
+
+        // sheet for pouch
+        flyCraft.generateSheet({
+            key: 'sheet_craft'
+        });
+
+        // sprite for pouch
+        var craft = game.data.create.craft = game.add.sprite(32 * 6, 64, 'sheet_craft', 0);
+        craft.name = 'craft';
+        craft.inputEnabled = true;
+
+        // pouch events
+        craft.events.onInputDown.add(function (craft, pt) {
+
+            // x, and y relative to pouch
+            var x = pt.x - craft.x,
+            y = pt.y - craft.y;
+
+            console.log(x, y);
+            console.log(craft.name);
 
         });
 
@@ -45,6 +101,7 @@
         create: function () {
 
             mkPouch();
+            mkCraft();
 
         }
 
